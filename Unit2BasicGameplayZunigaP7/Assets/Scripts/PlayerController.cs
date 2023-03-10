37fde7b742878d157ff5899ff8cd806a0481b2be
Pontius6 <716967@student.dusd.net>
2023-03-10 15:27:03 -0800

    new
    added vertical movement

--- Unit2BasicGameplayZunigaP7/Assets/Scripts/PlayerController.cs
+++ Unit2BasicGameplayZunigaP7/Assets/Scripts/PlayerController.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float verticalInput;
     public float horizontalInput;
     public float speed = 10.0f;
     public float xRange = 10;
+    public float zRange = 10;
 
     public GameObject projectilePrefab;
 
@@ -29,12 +31,28 @@ public class PlayerController : MonoBehaviour
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
 
+        if (transform.position.x < -zRange)
+        {
+            transform.position = new Vector3(-zRange, transform.position.x, transform.position.y);
+        }
+      
+        if (transform.position.x > zRange)
+        {
+            transform.position = new Vector3(zRange, transform.position.x, transform.position.y);
+        }
+
+        
+
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+
         }
     }
 }
